588e7a4a468dadbbb0d6329ccc2defb9e06425bc
Kaitlin Regan <karegan@gmail.com>
2020-10-14 12:55:18 -0400

    done

---
+++ lib/artist.rb
@@ -0,0 +1,40 @@
+class Artist
+    attr_accessor :name, :songs
+    @@all = []
+    @@song_count = 0
+    def initialize (name)
+        @name = name
+        @@all <<self
+        @songs = []
+    end
+    
+    def self.all
+        @@all
+    end
+
+    def songs
+        Song.all.select do |song|
+            song.artist == self
+        end
+    end
+
+    def add_song(song)
+        @@song_count += 1
+        @songs <<song
+        song.artist = self
+    end
+
+    def self.find_or_create_by_name(name)
+        self.find_by_name(name) || self.create_by_name(name)
+    end
+
+    def self.find_by_name(name)
+        self.all.find { |artist| artist.name = name}
+    end
+    def self.create_by_name(name)
+        self.new(name)
+    end
+    def print_songs
+        songs.each { |song| puts song.name}
+    end
+end
